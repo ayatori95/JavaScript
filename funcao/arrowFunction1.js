7b577d19aa518a8341f6ec4b7e0723bdbb957557
Ayatori <lorenacristinaac@gmail.com>
2022-05-01 14:04:30 -0300

    Arrow Function

---
+++ funcao/arrowFunction1.js
@@ -0,0 +1,10 @@
+let dobro = function (a) {
+    return 2 * a
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = a => 2 * a // funções de única linha
+console.log (dobro(Math.PI))
---
+++ funcao/arrowfFunction2.js
@@ -0,0 +1,12 @@
+// não roda, por que?
+
+function Pessoa() {
+    this.idade = 0
+
+    setInterval(() => {
+        this.idade++
+        console.log(this.idade)
+    }, 1000)
+}
+
+new Pessoa
