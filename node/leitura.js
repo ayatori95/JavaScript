6aa246454d0d96da67b85e254fd0cd07714bed6a
Ayatori <lorenacristinaac@gmail.com>
2022-06-15 15:12:54 -0300

    Lendo arquivos

--- node/leitura.js
+++ node/leitura.js
@@ -12,3 +12,6 @@ fs.readFile(caminho, 'utf-8', (err,conteudo) => {
     const config = JSON.parse(conteudo)
     console.log(`${config.db.host}:${config.db.port}`)
 })
+
+const config = require('./arquivo.json')
+console.log(config)
