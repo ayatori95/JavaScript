c1237d736a6ca972b145d54d2ebe1099faa288be
Ayatori <lorenacristinaac@gmail.com>
2022-06-17 11:26:45 -0300

    Protocolos HTTP get, post, delete, put

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,10 +2,27 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParse = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParse.urlencoded({extended:true}))
 
 app.get('/produtos', (req, res,next) => { // caso utilizar use no lugar de get, qualquer url irá responder a requisição
-    res.send({nome:'Notebook', preco:1345.45}) //Converte para JSON
+    res.send(bancoDeDados.getProdutos()) //Converte para JSON
 })
+
+app.get('/produtos/:id', (req,res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
 
 app.listen(porta, () =>{
     console.log(`Servidor execultando na porta ${porta}`)
