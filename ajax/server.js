b768c6bac036f7815bcdf1f7acfadfc6afe1f48a
Ayatori <lorenacristinaac@gmail.com>
2022-08-09 16:41:01 -0300

    Ajax

--- ajax/server.js
+++ ajax/server.js
@@ -6,5 +6,15 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send(new Date))
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
 app.listen(8080, ()=> console.log('Execultando'))
