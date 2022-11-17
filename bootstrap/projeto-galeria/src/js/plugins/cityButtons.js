6b4a61abd131f40031ba915d6291d958f3842d67
Ayatori <lorenacristinaac@gmail.com>
2022-11-17 16:57:17 -0300

    Galeria de Fotos

---
+++ bootstrap/projeto-galeria/src/js/plugins/cityButtons.js
@@ -0,0 +1,36 @@
+import $ from 'jquery'
+
+const duration = 600
+
+function filterByCity(city) {
+    $('[wm-city]').each( function(i,e){
+        const isTarget = $(this).attr('wm-city') === city
+            || city === null
+        if(isTarget) {
+            $(this).fadeIn(duration)
+        } else {
+            $(this).fadeOut(duration)
+        }
+    })
+}
+
+const cityButtons = $('[wm-city-buttons]')
+const cities = new Set
+$('[wm-city]').each( function(i,e){
+    cities.add($(e).attr('wm-city'))
+})
+
+const btns = Array.from(cities).map(city => {
+    const btn = $('<button>').addClass(['btn', 'btn-info']).html(city)
+    btn.click(e => filterByCity(city))
+    return btn
+})
+
+const btnAll = $('<button>').addClass(['btn', 'btn-info', 'active']).html('Todas')
+btnAll.click(e => filterByCity(null))
+btns.push(btnAll)
+
+const btnGroup = $('<div>').addClass(['btn-group'])
+btnGroup.append(btns)
+
+cityButtons.html(btnGroup)
