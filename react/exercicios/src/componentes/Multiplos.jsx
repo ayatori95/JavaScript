3116df05f6f03494b997ed441c6abeac46f33b42
Ayatori <lorenacristinaac@gmail.com>
2022-12-12 15:02:38 -0300

    Multiplos Componentes - React

---
+++ react/exercicios/src/componentes/Multiplos.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const BoaTarde = props => <h1>Boa noite {props.nome}</h1>
+
+export const BomDia2 = props => <h1>Bom Dia {props.nome}</h1>
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {BoaTarde, BomDia2}
--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './componentes/Primeiro'
-import BomDia from './componentes/BomDia'
+//import Primeiro from './componentes/Primeiro'
+//import BomDia from './componentes/BomDia'
+import Multi, { BoaTarde } from './componentes/Multiplos'
 
 
-ReactDOM.render(<BomDia nome="Lorena" />, document.getElementById('root'))
+// funciona nas duas formas
+ReactDOM.render(
+    <div>
+      <BoaTarde nome='Ana'></BoaTarde> 
+      <Multi.BomDia2 nome='Claúdia'/>
+    </div>
+, document.getElementById('root'))
