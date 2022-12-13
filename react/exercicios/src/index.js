58f1ac010ca5285247fcba918d800dbda40ec22e
Ayatori <lorenacristinaac@gmail.com>
2022-12-13 15:06:59 -0300

    Pai e Filho - React

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
 //import Primeiro from './componentes/Primeiro'
 //import BomDia from './componentes/BomDia'
 import Multi, { BoaTarde } from './componentes/Multiplos'
 import Saudacao from './componentes/Saudacao'
 import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 // funciona nas duas formas
 ReactDOM.render(
@@ -19,5 +21,8 @@ ReactDOM.render(
 <div>
   <Pai nome="Marcus" sobrenome =" O'Connel">
 {/* Como passar os componentes Filhos direto pela chamada? */}
+    <Filho nome="Tyler" />
+    <Filho nome="Elijah" />
+    <Filho nome="Juniper" />
   </Pai>
 </div>, document.getElementById('root2'))
