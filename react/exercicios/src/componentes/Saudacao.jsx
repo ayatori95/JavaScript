57aa8eac257e3b71706222993c30854fecfcf0c6
Ayatori <lorenacristinaac@gmail.com>
2022-12-13 13:43:10 -0300

    Componente Classe - React

--- react/exercicios/src/componentes/Saudacao.jsx
+++ react/exercicios/src/componentes/Saudacao.jsx
@@ -7,6 +7,13 @@ export default class Saudacao extends Component {
         nome: this.props.nome
     }
 
+    // eslint-disable-next-line no-useless-constructor
+    constructor (props) {
+        super(props)
+
+        this.setTipo = this.setTipo.bind(this)
+    }
+
     setTipo(e) {
         this.setState({tipo: e.target.value})
     }
@@ -20,7 +27,7 @@ export default class Saudacao extends Component {
             <div>
                 <h1>{tipo} {nome}!</h1>
                 <hr  />
-                <input type='text' placeholder='Tipo..' value={tipo} onChange={e => this.setTipo(e)}/>
+                <input type='text' placeholder='Tipo..' value={tipo} onChange={this.setTipo}/>
                 <input type='text' placeholder='Nome' value={nome} onChange={e => this.setNome(e)}/>
             </div>
         )
