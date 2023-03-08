10a9fe6c7cdd796befe6a519ff95bd049e168d66
Ayatori <lorenacristinaac@gmail.com>
2023-03-08 11:29:12 -0300

    Componente Conteúdo e Navegação

--- react/crud/frontend/src/components/template/Nav.jsx
+++ react/crud/frontend/src/components/template/Nav.jsx
@@ -2,6 +2,13 @@ import "./Nav.css"
 import React from "react"
 
 export default props =>
- <nav className="nav">
-    Navegação
- </nav>
+   <aside className="menu-area">
+      <nav className="menu">
+         <a href="#/">
+            <i className="fa fa-home"></i> Início
+         </a>
+         <a href="#/users">
+            <i className="fa fa-users"></i> Usuários
+         </a>
+      </nav>
+   </aside>
--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -5,7 +5,7 @@ import React from "react"
 
 import Logo from "../components/template/Logo"
 import Nav from "../components/template/Nav"
-import Main from "../components/template/Main"
+import Home from "../components/home/Home"
 import Footer from "../components/template/Footer"
 
 export default props =>
@@ -13,6 +13,6 @@ export default props =>
     <div className="app">
         <Logo></Logo>
         <Nav></Nav>
-        <Main icon="home" title ="Início" subtitle="Segundo Projeto do Capitulo de React."></Main>
+        <Home></Home>
         <Footer></Footer>
     </div>
